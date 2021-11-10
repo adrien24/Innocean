8cd3dc061c51d3813cd8483bb94dc93a29954736
adrien24 <59483650+adrien24@users.noreply.github.com>
2021-11-10 15:07:43 +0100

    Create jour.vue

---
+++ src/components/jour.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="jour">
+
+
+
+    {{moment().format('MMMM Do YYYY, h:mm:ss a')}}
+
+
+
+  </div>
+</template>
+
+<script>
+
+import moment from 'moment'
+
+
+export default {
+  name: "jour",
+
+  methods: {
+    moment: function () {
+      return moment();
+    }
+  },
+
+}
+
+
+
+</script>
+
+
+<style scoped>
+
+</style>
