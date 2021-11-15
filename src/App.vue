b82b1a8f7dc72355d0f5f236bd4a9f23f445eacf
adrien24 <59483650+adrien24@users.noreply.github.com>
2021-11-15 12:02:22 +0100

    up

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,11 @@
   <div id="app">
 
     <img src="https://source.unsplash.com/random" width="100px" height="100px">
-
     <router-link to="/">Accueil</router-link>
     <router-link to="/about">About</router-link>
-
-    <Marque/>
     <FooterP/>
     <jour />
-    {{currentDateTime()}}
+
 
 
     <router-view/>
@@ -19,10 +16,9 @@
 <script>
 
 import FooterP from "./components/FooterP";
-import Marque from "./components/Marquee";
 import GoogleMap from './components/GoogleMap.vue'
 import jour from './components/jour'
-import moment from 'moment'
+
 
 
 
@@ -30,17 +26,11 @@ export default {
   name: 'App',
   components: {
     FooterP,
-    Marque,
     GoogleMap,
     jour
   },
 
-  methods: {
-    currentDateTime() {
-      return moment().format('MMMM Do YYYY, h:mm:ss a')
-    }
 
-  },
 
 
 
